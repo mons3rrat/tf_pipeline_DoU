a9002ffae0484d92d2abb006eb3a43eb2b8f59d3
Gonzalo Lopez <gonzalo.lopez@digitalonus.com>
2018-11-19 10:24:49 -0600

    ADD Java Application - Jenkins: Deploy docker application on AWS

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
+terraform {
+  backend "s3" {
+    bucket = "tf-remote-state-training-nov"
+    key = "tf_pipeline_DoU/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
