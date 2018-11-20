5aef042e56fc896f175458507ba6ada7559e6112
mons3rrat <msede25@gmail.com>
2018-11-20 10:05:04 -0600

    UPDATE aws secuirity group

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name = "deployer-key-sedenom"
+  key_name = "deployer-key"
   public_key = "${file(var.my_public_key_path)}"
 }
 
